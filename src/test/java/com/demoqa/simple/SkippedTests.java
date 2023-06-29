5f0412066f167cbfb9604eda1ea6c6e29c5e9db0
Nastya <kolod.02@mail.ru>
2023-06-30 03:24:43 +0700

    added remote test

--- src/test/java/com/demoqa/simple/SkippedTests.java
+++ src/test/java/com/demoqa/simple/SkippedTests.java
@@ -1,12 +1,11 @@
 package com.demoqa.simple;
 
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@Tag("simple")
+//@Tag("simple")
 public class SkippedTests {
 
     @Test
